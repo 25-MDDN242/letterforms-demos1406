8d50a21db111ba1462c620c74ac10205e39cc3b4
helena <109050427+demos1406@users.noreply.github.com>
2025-03-31 12:27:46 +1300

    glyph idea
    first idea, scrapping

--- sketch.js
+++ sketch.js
@@ -13,36 +13,39 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  coreType: 1, // 0 = circle, 1 = triangle, 2 = square
+  // coreType: 1, // 0 = circle, 1 = triangle, 2 = square
   coreSize: 80,
-  numRays: 3,
-  orbitalSpread: 60,
-  orbitalAngle: 60
+  secondCoreSize: 70,
+  // numRays: 3,
+  // orbitalSpread: 60,
+  // orbitalAngle: 60
 }
 
 const letterB = {
-  coreType: 0,
+  // coreType: 0,
   coreSize: 100,
-  numRays: 8,
-  orbitalSpread: 90,
-  orbitalAngle: 0
+  secondsCoreSize: 90,
+  // numRays: 8,
+  // orbitalSpread: 90,
+  // orbitalAngle: 0
 }
 
 const letterC = {
-  coreType: 2,
+  // coreType: 2,
   coreSize: 60,
-  numRays: 2,
-  orbitalSpread: 60,
-  orbitalAngle: 90
+  secondCoreSize: 50,
+  // numRays: 2,
+  // orbitalSpread: 60,
+  // orbitalAngle: 90
 }
 
 
 
-const backgroundColor  = "#001d3d";
+const backgroundColor  = "#d9d6dd";
 
-const darkBlue  = "#003566";
-const yellowColor  = "#ffc300";
-const strokeColor  = "#ffd60a";
+const blueColor  = "#324dd4";
+const pumpkin  = "#fd893e";
+const pinkColor  = "#ff41ae";
 
 
 function setup () {
@@ -51,8 +54,8 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   // color/stroke setup
-  stroke(strokeColor);
-  strokeWeight(4);
+  // stroke(blueColor);
+  // strokeWeight(4);
 
 
   // with no animation, redrawing the screen is not necessary
@@ -79,56 +82,66 @@ function drawLetter(posx, posy, letterData) {
   let orbitalSpread = letterData["orbitalSpread"]
   let orbitalAngle = letterData["orbitalAngle"]
   let coreType = letterData["coreType"]
+  let secondCoreSize = letterData["secondCoreSize"]
 
   // core circle
-  fill(darkBlue);
-  stroke(strokeColor)
   strokeWeight(3);
-  // ellipse(posx, posy, coreSize);
+  fill(pinkColor)
+  stroke(blueColor)
 
-  if (coreType == 0) {
-    // circle
-    ellipse(posx, posy, coreSize);
-  }
-  else if (coreType == 1) {
-    // triangle
-    beginShape(TRIANGLES)
-    let r = coreSize / 2;
-    for (let i = 0; i < 3; i++) {
-      let angle = TWO_PI * i / 3; // equilateral triangle
-      let vx = posx + cos(angle) * r;
-      let vy = posy + sin(angle) * r;
-      vertex(vx, vy);
-    }
-    endShape(CLOSE);
-  }
-  else if (coreType == 2) {
-    // square
-    rect(posx - coreSize / 2, posy - coreSize / 2, coreSize, coreSize);
-  }
+  fill(pinkColor)
+  ellipse(posx, posy, coreSize);
+  noStroke()
+  fill(50, 77, 212, 150)
+  ellipse(posx + 30, posy + 30, secondCoreSize);
 
-  // radial rays
-  // math was assited by ChatGPT
-  for (let i = 0; i < numRays; i++) {
-    let angle = TWO_PI * i / numRays;
-    let x1 = posx + cos(angle) * (coreSize / 2);
-    let y1 = posy + sin(angle) * (coreSize / 2);
-    let x2 = posx + cos(angle) * (coreSize / 2 + 30);
-    let y2 = posy + sin(angle) * (coreSize / 2 + 30);
-    line(x1, y1, x2, y2);
-  }
 
-  // // orbiting dots
+
+  // if (coreType == 0) {
+  //   // circle
+    
+    
+  // }
+  // else if (coreType == 1) {
+  //   // triangle
+  //   beginShape()
+  //   let r = coreSize / 2;
+  //   for (let i = 0; i < 3; i++) {
+  //     let angle = TWO_PI * i / 3; // equilateral triangle
+  //     let vx = posx + cos(angle) * r;
+  //     let vy = posy + sin(angle) * r;
+  //     vertex(vx, vy);
+  //   }
+  //   endShape(CLOSE);
+  // }
+  // else if (coreType == 2) {
+  //   // square
+  //   rect(posx - coreSize / 2, posy - coreSize / 2, coreSize, coreSize);
+  // }
+
+  // stroke(blueColor)
+  // // radial rays
   // // math was assited by ChatGPT
-  let baseAngle = radians(orbitalAngle); // convert from degrees
-  for (let i = 0; i < 3; i++) {
-    let angle = TWO_PI * i / 3 + baseAngle;
-    let orbX = posx + cos(angle) * orbitalSpread;
-    let orbY = posy + sin(angle) * orbitalSpread;
-    noStroke();
-    fill("#ffaa33");
-    ellipse(orbX, orbY, 10);
-  }
+  // for (let i = 0; i < numRays; i++) {
+  //   let angle = TWO_PI * i / numRays;
+  //   let x1 = posx + cos(angle) * (coreSize / 2);
+  //   let y1 = posy + sin(angle) * (coreSize / 2);
+  //   let x2 = posx + cos(angle) * (coreSize / 2 + 30);
+  //   let y2 = posy + sin(angle) * (coreSize / 2 + 30);
+  //   line(x1, y1, x2, y2);
+  // }
+
+  // // // orbiting dots
+  // // // math was assited by ChatGPT
+  // let baseAngle = radians(orbitalAngle); // convert from degrees
+  // for (let i = 0; i < 3; i++) {
+  //   let angle = TWO_PI * i / 3 + baseAngle;
+  //   let orbX = posx + cos(angle) * orbitalSpread;
+  //   let orbY = posy + sin(angle) * orbitalSpread;
+  //   noStroke();
+  //   fill(pumpkin);
+  //   ellipse(orbX, orbY, 10);
+  // }
 }
 
 
